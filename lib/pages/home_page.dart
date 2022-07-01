e003b8fdfc455bf6b7bac14f9c50337973be08c0
Hanri <henry018@laposte.net>
2022-07-01 15:12:29 +0200

    home page with multiple views

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,14 +1,55 @@
 import 'package:flutter/material.dart';
 import 'package:meals_app/views/categories_view.dart';
+import 'package:meals_app/views/favorites_view.dart';
 
-class HomePage extends StatelessWidget {
+class HomePage extends StatefulWidget {
   const HomePage({Key? key}) : super(key: key);
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+
+  int currentPage = 0;
+
+  final List<Map<String, Object>> pages = const [
+    {'page': CategoriesView(), 'title': "Categories"},
+    {'page': FavoritesView(), 'title': "Favorite"},
+  ];
+
+  void selectPage(int index) {
+    setState(() {
+      currentPage = index;
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: const Text("Meals App"),),
-      body: const CategoriesView()
+      appBar: AppBar(
+        title: Text(pages[currentPage]['title'] as String),
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        unselectedItemColor: Colors.white,
+        //backgroundColor: Theme.of(context).primaryColor,
+        selectedItemColor: Theme.of(context).colorScheme.secondary,
+        onTap: selectPage,
+        currentIndex: currentPage,
+        type: BottomNavigationBarType.shifting,
+        items: [
+          bottomNavItem(icon: Icons.category, label: "Categories"),
+          bottomNavItem(icon: Icons.star, label: "Favorites",),
+        ],
+      ),
+      body: pages[currentPage]['page'] as Widget
+    );
+  }
+  BottomNavigationBarItem bottomNavItem({required IconData icon, required String label}) {
+    return BottomNavigationBarItem(
+      backgroundColor: Theme.of(context).primaryColor,
+      icon: Icon(icon),
+      label: label
     );
   }
 }
